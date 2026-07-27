32b14a43346059b572bc972c4cb71e27056fb1fa
hoakiin <millychak89@gmail.com>
2026-07-27 16:18:49 +0300

    SCRUM-60 fix: add forwardRef support to Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,5 @@
-import type { InputHTMLAttributes, ReactNode } from 'react'
 import clsx from 'clsx'
+import { forwardRef, type InputHTMLAttributes, type ReactNode } from 'react'
 import { Typography } from '../Typography/Typography'
 import s from './Input.module.scss'
 
@@ -10,39 +10,37 @@ export type InputProps = {
   endAdornment?: ReactNode
 } & InputHTMLAttributes<HTMLInputElement>
 
-export const Input = ({
-  label,
-  error,
-  startAdornment,
-  endAdornment,
-  className,
-  disabled,
-  ...props
-}: InputProps) => {
-  return (
-    <div className={s.wrapper}>
-      {label && <label className={s.label}>{label}</label>}
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, startAdornment, endAdornment, className, disabled, ...props }, ref) => {
+    return (
+      <div className={s.wrapper}>
+        {label && <label className={s.label}>{label}</label>}
 
-      <div
-        className={clsx(s.container, {
-          [s.error]: !!error,
-          [s.disabled]: disabled,
-        })}
-      >
-        {startAdornment && (
-          <span className={clsx(s.adornment, s.startAdornment)}>{startAdornment}</span>
+        <div
+          className={clsx(s.container, {
+            [s.error]: !!error,
+            [s.disabled]: disabled,
+          })}
+        >
+          {startAdornment && (
+            <span className={clsx(s.adornment, s.startAdornment)}>{startAdornment}</span>
+          )}
+
+          <input ref={ref} className={clsx(s.input, className)} disabled={disabled} {...props} />
+
+          {endAdornment && (
+            <span className={clsx(s.adornment, s.endAdornment)}>{endAdornment}</span>
+          )}
+        </div>
+
+        {error && (
+          <Typography variant={"body1"} color={"var(--color-danger-500)"}>
+            {error}
+          </Typography>
         )}
-
-        <input className={clsx(s.input, className)} disabled={disabled} {...props} />
-
-        {endAdornment && <span className={clsx(s.adornment, s.endAdornment)}>{endAdornment}</span>}
       </div>
+    )
+  }
+)
 
-      {error && (
-        <Typography variant={'body1'} color={'var(--color-danger-500)'}>
-          {error}
-        </Typography>
-      )}
-    </div>
-  )
-}
+Input.displayName = 'Input'
