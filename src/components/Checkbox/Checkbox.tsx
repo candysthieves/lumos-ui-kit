b3c7b79fdd0a3fc54aa8dec3e280f554ae04ee36
hoakiin <millychak89@gmail.com>
2026-07-27 16:10:47 +0300

    SCRUM-69 feat: improve checkbox component

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -22,25 +22,23 @@ export const Checkbox = forwardRef<
 
   return (
     <div className={s.container}>
-      <label htmlFor={checkboxId} className={s.buttonWrapper}>
-        <CheckboxPrimitive.Root
-          ref={ref}
-          id={checkboxId}
-          className={clsx(s.root, className)}
-          onCheckedChange={checked => onCheckedChange?.(checked === true)}
-          {...props}
-        >
-          <CheckboxPrimitive.Indicator className={s.indicator}>
-            <CheckmarkOutline autoSize={false} color={"var(--color-dark-900)"} size={30} />
-          </CheckboxPrimitive.Indicator>
-        </CheckboxPrimitive.Root>
-      </label>
+      <label htmlFor={checkboxId} className={s.label}>
+        <span className={s.buttonWrapper}>
+          <CheckboxPrimitive.Root
+            ref={ref}
+            id={checkboxId}
+            className={clsx(s.root, className)}
+            onCheckedChange={checked => onCheckedChange?.(checked === true)}
+            {...props}
+          >
+            <CheckboxPrimitive.Indicator className={s.indicator}>
+              <CheckmarkOutline color={"var(--color-dark-900)"} />
+            </CheckboxPrimitive.Indicator>
+          </CheckboxPrimitive.Root>
+        </span>
 
-      {label && (
-        <label htmlFor={checkboxId} className={s.label}>
-          {label}
-        </label>
-      )}
+        {label && <span>{label}</span>}
+      </label>
     </div>
   )
 })
