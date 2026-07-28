1aa86e36f6566d2bafdfb5b971e5b3bc250a7d52
Intrstng <intrstg@gmail.com>
2026-07-28 19:28:23 +0300

    SCRUM-49 fix: add exports for components from library

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,8 @@
 export * from './Typography/Typography'
 export * from './TestMixins/TestMixins'
 export * from './RadioGroup/RadioGroup'
+export * from './SearchInput/SearchInput'
+// With controller:
+export * from './Input/FormInput'
+export * from './Checkbox/FormCheckbox'
+export * from './PasswordInput/FormPasswordInput'
