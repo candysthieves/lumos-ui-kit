5c4926f1f2c4a78fed2f068fd0e7939bdf463e71
Andrei <107413715+Intrstng@users.noreply.github.com>
2026-07-29 18:53:44 +0300

    SCRUM-56: add Button and ButtonLegacy components with Storybook
    * SCRUM-56 feat: add a styled Button component (ref support via props - React19)
    
    * SCRUM-56 feat: add StoryBook to Button component
    
    * SCRUM-56 feat: add ButtonLegacy component (using ref handling with forwardRef for React below v.19)
    
    * SCRUM-56 feat: add ButtonLegacy Storybook
    
    * SCRUM-00 chore: update develop branch
    
    * SCRUM-56 style: change button border styles
    
    * SCRUM-56 fix: change 'as' description in Storybook for Button and ButtonLegacy components
    
    * SCRUM-56 fix: resolve PR feedback
    
    * SCRUM-56 fix: resolve PR Storybook feeadback
    
    * SCRUM-56 style: resolve PR SCSS feedback

--- src/storybook-utils/index.ts
+++ src/storybook-utils/index.ts
@@ -1,3 +1,4 @@
 export { iconMeta } from '@/storybook-utils/assets/iconMeta'
 export { coloredIconMeta } from '@/storybook-utils/assets/coloredIconMeta'
 export { logoIconMeta } from '@/storybook-utils/assets/logoMeta'
+export { buttonMeta } from '@/storybook-utils/components/ButtonMeta'
