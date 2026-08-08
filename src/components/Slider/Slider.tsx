9e2011b0fcc2f7ae8e68ea799fa8f7ffeccc0ec0
Vlad Skalski <vlad.skalsky.96@gmail.com>
2026-08-08 16:54:00 +0200

    SCRUM-82 style: use shared mixins in Slider styles

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,5 +1,6 @@
 import type { ComponentPropsWithoutRef, ComponentRef } from 'react'
 import * as SliderPrimitive from '@radix-ui/react-slider'
+import clsx from 'clsx'
 import { forwardRef } from 'react'
 import s from './Slider.module.scss'
 
@@ -9,9 +10,9 @@ export type SliderProps = Omit<
 >
 type SliderElement = ComponentRef<typeof SliderPrimitive.Root>
 
-export const Slider = forwardRef<SliderElement, SliderProps>((props, ref) => {
+export const Slider = forwardRef<SliderElement, SliderProps>(({ className, ...props }, ref) => {
   return (
-    <SliderPrimitive.Root ref={ref} className={s.root} {...props}>
+    <SliderPrimitive.Root ref={ref} className={clsx(s.root, className)} {...props}>
       <SliderPrimitive.Track className={s.track}>
         <SliderPrimitive.Range className={s.range} />
       </SliderPrimitive.Track>
