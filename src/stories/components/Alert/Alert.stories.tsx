bb58c3419760d5f5fe02403fe698ebf984e4cff3
Vlad Skalski <vlad.skalsky.96@gmail.com>
2026-08-10 11:34:39 +0200

    SCRUM-81 refactor: refactor imports

--- src/stories/components/Alert/Alert.stories.tsx
+++ src/stories/components/Alert/Alert.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
-import { Alert } from '../../../components/Alert'
+import { Alert } from '@/components/Alert'
 
 const meta = {
   title: 'Components/Alert',
